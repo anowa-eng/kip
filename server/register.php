cf413ede82b548362be13358e15f410725e38a68
anova02 <georgemorrissette2010@gmail.com>
2023-07-22 22:26:06 -0700

    Create basic registry system

--- server/register.php
+++ server/register.php
@@ -4,6 +4,6 @@ $user_dir = "$registry_dir/{$_POST['user']}";
 $repository_dir = "$user_dir/{$_POST['repo']}.kip";
 
 if (!is_dir($user_dir))
-    mkdir("$registry_dir/{$_POST['username']}");
+    mkdir($user_dir);
 
 file_put_contents($repository_dir, $_FILES['file']);
