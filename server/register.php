0c0617827e0991700e321658860e4cb641956cff
anova02 <georgemorrissette2010@gmail.com>
2023-07-24 19:57:18 -0700

    Install josantonius/session

--- server/register.php
+++
@@ -1,9 +0,0 @@
-<?php
-$registry_dir = __DIR__ . '/registry';
-$user_dir = "$registry_dir/{$_POST['user']}";
-$repository_dir = "$user_dir/{$_POST['repo']}.kip";
-
-if (!is_dir($user_dir))
-    mkdir($user_dir);
-
-file_put_contents($repository_dir, $_FILES['file']);
