4d476d8281d1322d05ad571b4d76eb5a790193e5
anova02 <georgemorrissette2010@gmail.com>
2023-07-26 21:55:00 -0700

    Move route, add existence check, creation & more

--- server/db.py
+++ server/db.py
@@ -1,26 +1,38 @@
 from pathlib import Path
 import sqlite3
 
+COLS = {
+    'Users': 2,
+    'Repositories': 4
+}
+TABLES = ('Users', 'Repositories')
+
 db_path = Path(__file__).parent / 'db.sqlite3'
 def init():
     Path.touch(db_path)
-    cursor = sqlite3.connect(db_path)
+    connection = sqlite3.connect(db_path)
     users_table = '''
     CREATE TABLE IF NOT EXISTS Users (
-        ID int NOT NULL PRIMARY KEY AUTOINCREMENT,
-        Username varchar(255) NOT NULL,
+        Username varchar(255) NOT NULL PRIMARY KEY,
         Password text NOT NULL
     );
     '''
     repositories_table = '''
     CREATE TABLE IF NOT EXISTS Repositories (
-        ID int NOT NULL PRIMARY KEY AUTOINCREMENT,
-        FullRepositoryName text NOT NULL,
+        FullRepositoryName text NOT NULL PRIMARY KEY,
         AuthorUsername varchar(255),
         RepositoryName varchar(255),
         ModelFilePath text,
         FOREIGN KEY (AuthorUsername) REFERENCES Users(Username)
     );
     '''
-    cursor.execute(users_table)
-    cursor.execute(repositories_table)
+    connection.execute(users_table)
+    connection.execute(repositories_table)
+    connection.commit()
+
+def create(table, values):
+    connection = sqlite3.connect(db_path)
+    stmt = f'''
+    INSERT INTO {table} VALUES ({", ".join("?" * COLS[table])})
+    '''
+    return connection.execute(stmt, values)
--- server/main.py
+++ server/main.py
@@ -1,6 +1,6 @@
 import sys
 from pathlib import Path
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_session import Session
 import db
 
@@ -11,14 +11,25 @@ app = Flask(__name__)
 app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
 
-@app.get('/<user>/<repo>')
+@app.get('/registry/<user>/<repo>')
 def repository_download(user, repo):
     repository_path = Path(__file__).parent / f'registry/{user}/{repo}.kip'
-    with open(repository_path, 'r') as reader:
-        response = app.make_response(reader.read())
-        response.headers['Content-Type'] = 'application/octet-stream'
-        response.headers['Content-Disposition'] = 'attachment'
-        return response
+    print(str(repository_path))
+    if Path.exists(repository_path):
+        with open(repository_path, 'r') as reader:
+            response = app.make_response(reader.read())
+            response.headers['Content-Type'] = 'application/octet-stream'
+            response.headers['Content-Disposition'] = 'attachment'
+            return response
+    else:
+        return jsonify({ 'error': 'Repository does not exist.' })
+    
+@app.post('/registry/<user>/<repo>')
+def repository_create(user, repo):
+    repository_path = Path(__file__).parent / f'registry/{user}/{repo}.kip'
+    repository_path.write_bytes(request.files[0])
+    cursor = db.create('Repositories', (f'{user}/{repo}', user, repo, ''))
+    return jsonify(cursor.fetchone())
 
 if __name__ == '__main__':
     app.run()
