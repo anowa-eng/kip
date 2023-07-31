abce647bc0c1f64cd5872a14c990ccfa34409a1c
anova02 <georgemorrissette2010@gmail.com>
2023-07-30 18:59:08 -0700

    Create setup.py in setup directory

---
+++ setup/setup.py
@@ -0,0 +1,13 @@
+import json
+from pathlib import Path
+import sys
+sys.path.append(str(Path.home() / ".kip/lib"))
+import const
+
+print(f"Creating s\u001b[1m{str(const.KIP_HOME_DIRECTORY)}\u001b[0m...")
+const.KIP_HOME_DIRECTORY.mkdir()
+print("Creating configuration file...")
+const.KIP_CONFIGURATIONS_FILE.touch()
+const.KIP_CONFIGURATIONS_FILE.write_text(json.dumps(const.KIP_DEFAULT_CONFIGURATIONS))
+
+print("Setup complete.")
