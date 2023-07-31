59eb9923b8fcc702fb9593af9e6e4a5872971c54
anova02 <georgemorrissette2010@gmail.com>
2023-07-30 19:04:35 -0700

    Remove `__kebab_case_to_camel_case`

--- cli/functions.py
+++ cli/functions.py
@@ -4,25 +4,13 @@ from pathlib import Path
 sys.path.append(str(Path.home() / ".kip/lib"))
 import const
 
-def __kebab_case_to_camel_case(string):
-    n_string = [*string]
-    while '-' in n_string:
-        i = n_string.index('-')
-        if i == len(string) - 1:
-            del n_string[i]
-        else:
-            n_string[i] = str.upper(n_string[i + 1])
-            del n_string[i + 1]
-    n_string = ''.join(n_string)
-    return n_string
-
-def __set_config(**kwargs):
+def __set_config(args):
     try:
         with open(const.KIP_CONFIGURATIONS_FILE, 'r+') as reader:
             configurations = json.loads(reader.read())
             n_configurations = {
                 **configurations,
-                **kwargs
+                **args
             }
         return True
     except Exception as e:
@@ -32,4 +20,6 @@ def __set_config(**kwargs):
 def set_registry_url(url):
     if url[-1] == '/':
         url = url[0:-1]
-    return __set_config(defaultRegistry=url)
+    return __set_config({
+        'registry.url': url
+    })
