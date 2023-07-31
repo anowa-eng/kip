f56c0b2229ca31a2ff05d27fb08e6eba74f859b1
anova02 <georgemorrissette2010@gmail.com>
2023-07-30 18:56:47 -0700

    Update constants

--- cli/const.py
+++
@@ -1,4 +0,0 @@
-from pathlib import Path
-
-KIP_HOME_DIRECTORY = Path.home() / ".kip"
-KIP_CONFIGURATIONS_FILE = KIP_HOME_DIRECTORY / "config.json"
---
+++ const.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+KIP_HOME_DIRECTORY = Path.home() / ".kip"
+KIP_CONFIGURATIONS_FILE = KIP_HOME_DIRECTORY / "config.json"
+
+KIP_CONFIGURATIONS_KEYS = ['defaultRegistry']
+CLI_KIP_CONFIGURATIONS_ARGUMENTS = ['default-registry']
+
+KIP_DEFAULT_CONFIGURATIONS = {
+    "registry.url": "https://127.0.0.1"
+}
