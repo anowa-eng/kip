b3c7d3c630f262a86ee7593e99b45f7c8e04e1aa
anova02 <georgemorrissette2010@gmail.com>
2023-07-30 19:09:38 -0700

    Add `~/.kip/var` to installation process

--- setup/setup.py
+++ setup/setup.py
@@ -6,6 +6,8 @@ import const
 
 print(f"Creating s\u001b[1m{str(const.KIP_HOME_DIRECTORY)}\u001b[0m...")
 const.KIP_HOME_DIRECTORY.mkdir()
+print(f"Creating s\u001b[1m{str(const.KIP_HOME_DIRECTORY / 'var')}\u001b[0m...")
+const.KIP_VAR_DIRECTORY.mkdir()
 print("Creating configuration file...")
 const.KIP_CONFIGURATIONS_FILE.touch()
 const.KIP_CONFIGURATIONS_FILE.write_text(json.dumps(const.KIP_DEFAULT_CONFIGURATIONS))
